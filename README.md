d4fa742b5e358bffeaefd72239391798be132d0b
tahaluindo <94370774+tahaluindo@users.noreply.github.com>
2021-12-10 13:49:03 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ README.md
@@ -0,0 +1,61 @@
+# SPYRO SER V1
+<p align="center">
+<a href="#"><img title="SPARROW OFC" src="https://img.shields.io/badge/SPARROW-green?colorA=%23ff0000&colorB=%23017e40&style=for-the-badge"></a>
+</p>
+<p align="center">
+<a href="https://github.com/Sparrow"><img title="Author" src="https://img.shields.io/badge/AUTHOR-AKSHAY-orange.svg?style=for-the-badge&logo=github"></a>
+</p>
+<p align="center">
+<a href="https://github.com/sparroo/jarvis/followers"><img title="Followers" src="https://img.shields.io/github/followers/sparroo?color=blue&style=flat-square"></a>
+<a href="https://github.com/sparroo/jarvis/stargazers/"><img title="Stars" src="https://img.shields.io/github/stars/sparroo/jarvisv3?color=red&style=flat-square"></a>
+<a href="https://github.com/sparroo/jarvis/network/members"><img title="Forks" src="https://img.shields.io/github/forks/sparroo/jarvisv3?color=red&style=flat-square"></a>
+<a href="https://github.com/sparroo/jarvis/watchers"><img title="Watching" src="https://img.shields.io/github/watchers/sparroo/jarvisv3?label=Watchers&color=blue&style=flat-square"></a>
+<a href="https://hits.seeyoufarm.com"><img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https://github.com/sparroo/jarvisv3&count_bg=%2379C83D&title_bg=%23555555&icon=probot.svg&icon_color=%2300FF6D&title=hits&edge_flat=false"/></a>
+</p>
+</details>
+
+## THANKS TO SPYRO
+
+# CLONE THIS PROJECT  <img src="https://github.com/TheDudeThatCode/TheDudeThatCode/blob/master/Assets/hmm.gif" width="29px">
+
+### Via [`Termux `](https://f-droid.org/packages/com.termux/)
+```bash
+> Download termux app
+> termux-setup-storage
+> apt update && apt upgrade
+> pkg install git
+> pkg install bash
+> git clone https://github.com/spyrosir45/jarvisv3.git
+> cd jarvis
+> bash install.sh
+> npm start
+> SCAN THE QR CODE
+```
+
+### Via [`Heroku `](https://www.heroku.com)
+```bash
+> Fork this repostry to your github.
+> Add a session.json file.
+> Create a heroku account or login.
+> Create a new app.
+> Click the github icon.
+> Connect to your github account.
+> Deploy branch.
+> Start the worker or web. If you cant find a worker just refresh the page.
+> If error occurs click on new icon in the top left corner. Select run console and type node index.
+> ENJOY
+```
+
+### Even though heroku is available, I still recommend using termux for better results.
+
+# CONTACT ME <img src="https://github.com/TheDudeThatCode/TheDudeThatCode/blob/master/Assets/powerup.gif" width="29px">
+
+* [`WhatsApp Number `](https://wa.me/+918590299146)
+* [`WhatsApp Group `]( GROUP ELLA)
+
+
+# THANKS TO <img src="https://github.com/TheDudeThatCode/TheDudeThatCode/blob/master/Assets/Handshake.gif" width="60px">
+
+* [`SPARROW`]
+<img src="https://github.com/TheDudeThatCode/TheDudeThatCode/blob/master/Assets/Mario_Gameplay.gif" alt="Mario Game" width="600" />
+
---
+++ jarvis/akshay.js
@@ -0,0 +1,312 @@
+const petik = '```'
+
+const dtod = "918590299146@s.whatsapp.net"
+const help = (prefix, wita, wit, ucapannya, timuu, status, wa_version, mcc, mnc, os_version, device_manufacturer, device_model, alfa , alfa1, thisDay, ini_tanggal, totalchat, hit_today, ini_gcchat, latensiii, tekss) => {
+	return`
+┌───〔 ɪɴғᴏʀᴍᴀᴛɪᴏɴ 〕
+┊✰ᴄʀᴇᴀᴛᴏʀ : sᴘʏʀᴏ sᴇʀ
+┊✰ᴛɪᴍᴇ : ${timuu}
+┊✰ᴅᴀʏ : ${thisDay}
+┊✰ᴅᴀᴛᴇ : ${ini_tanggal}
+┊✰ᴡʜᴀᴛsᴀᴘᴘ : ${wa_version}
+┊✰ᴅᴇᴠɪᴄᴇ : ${device_manufacturer}
+┊✰ʀᴀᴍ : ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
+┊
+├───〔 ᴀʙᴏᴜᴛ 〕
+┊༆sᴛᴀᴛᴜs : ${status}
+┊༆ᴘʀᴇғɪx : ${prefix}
+┊༆ᴛᴏᴛᴀʟ ᴄᴍᴅ : ${hit_today.length}
+┊༆ᴘᴄ : ${totalchat.length - ini_gcchat.length}
+┊༆ᴛᴏᴛᴀʟ ᴄʜᴀᴛ : ${totalchat.length}
+┊༆ɢʀᴏᴜᴘ ᴄʜᴀᴛ : ${ini_gcchat}
+┊༆sᴇʀᴠᴇʀ : Baileys
+┊༆ᴠᴇʀsɪᴏɴ : 3.5.1
+┊༆sᴘᴇᴇᴅ :${latensiii} 
+┊
+├───〔 ᴛʜᴀɴᴋs ᴛᴏ 〕
+┊
+├〔 sᴘʏʀᴏ sᴇʀ 〕
+├〔 ᴀᴅɪᴛʜ sᴇʀ 〕
+├〔 ᴛᴏᴍ sᴇʀ 〕
+├〔 ᴊɪɴɴ sᴇʀ 〕
+└───〔 sᴘʏʀᴏ 〕
+`
+}
+const convert = (prefix) => {
+	return`
+┌───〔 *_sᴘʏʀᴏ_* 〕
+┊
+└───────────𔒝
+┌───〔 ᴄᴏɴᴠᴇʀᴛ ᴍᴇɴᴜ 〕
+┊✵${prefix}toimg
+┊✵${prefix}tomp3
+┊✵${prefix}tomp4
+┊✵${prefix}slow
+┊✵${prefix}fast
+┊✵${prefix}reverse
+┊✵${prefix}tourl
+└───────────𔒝
+`
+}
+const download = (prefix) => {
+	return`
+┌───〔 *_sᴘʏʀᴏ_* 〕
+┊
+└───────────𔒝
+┌───〔 ᴅᴏᴡɴʟᴏᴀᴅ ᴍᴇɴᴜ 〕
+┊𖣘${prefix}ytsearch
+┊𖣘${prefix}play
+┊𖣘${prefix}video
+┊𖣘${prefix}ytmp3
+┊𖣘${prefix}ytmp4
+┊𖣘${prefix}ig
+┊𖣘${prefix}image
+└───────────𔒝
+`
+}
+const allmenu = (prefix, wita, wit, ucapannya, timuu, status, wa_version, mcc, mnc, os_version, device_manufacturer, device_model, alfa , alfa1, thisDay, ini_tanggal, totalchat, hit_today, ini_gcchat, latensiii, tekss)  => {
+	return`
+┌───〔 *_sᴘʏʀᴏ_* 〕
+┊
+└───────────𔒝
+┌───〔 ɪɴғᴏʀᴍᴀᴛɪᴏɴ 〕
+┊༆ᴛɪᴍᴇ : ${timuu}
+┊༆ᴅᴀʏ : ${thisDay}
+┊༆ᴅᴀᴛᴇ : ${ini_tanggal}
+┊༆ᴡʜᴀᴛsᴀᴘᴘ : ${wa_version}
+┊༆ᴅᴇᴠɪᴄᴇ : ${device_manufacturer}
+┊༆ᴍᴏᴅᴇʟ : ${device_model}
+┊༆ʀᴀᴍ : ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
+└───────────𔒝
+┌───〔 ᴀʙᴏᴜᴛ 〕
+┊⍟sᴛᴀᴛᴜs : ${status}
+┊⍟ᴘʀᴇғɪx : ${prefix}
+┊⍟ᴛᴏᴛᴀʟ ᴄᴍᴅ : ${hit_today.length}
+┊⍟ᴘᴄ : ${totalchat.length - ini_gcchat.lenght}
+┊⍟ᴛᴏᴛᴀʟ ᴄʜᴀᴛ : ${totalchat.length}
+┊⍟ɢʀᴏᴜᴘ ᴄʜᴀᴛ : ${ini_gcchat}
+┊⍟sᴇʀᴠᴇʀ : Baileys
+┊⍟ᴠᴇʀsɪᴏɴ : 3.5.1
+┊⍟sᴘᴇᴇᴅ :${latensiii} 
+└───────────𔒝
+┌───〔 ᴄᴏɴᴠᴇʀᴛ ᴍᴇɴᴜ 〕
+┊🛠${prefix}toimg
+┊🛠${prefix}tomp3
+┊🛠${prefix}tomp4
+┊🛠${prefix}slow
+┊🛠${prefix}fast
+┊🛠${prefix}reverse
+┊🛠${prefix}tourl
+┊
+├───〔 ᴅᴏᴡɴʟᴏᴀᴅ ᴍᴇɴᴜ 〕
+┊📡${prefix}ytsearch
+┊📡${prefix}play
+┊📡${prefix}video
+┊📡${prefix}ytmp3
+┊📡${prefix}ytmp4
+┊📡${prefix}ig
+┊📡${prefix}image
+┊
+├───〔 ᴍᴀᴋᴇʀ ᴍᴇɴᴜ 〕
+┊🎐${prefix}sticker
+┊🎐${prefix}swm
+┊🎐${prefix}take
+┊🎐${prefix}fdeface
+┊🎐${prefix}emoji
+┊
+├───〔 ᴏᴛʜᴇʀ ᴍᴇɴᴜ 〕
+┊✨${prefix}add
+┊✨${prefix}ping
+┊✨${prefix}get
+┊✨${prefix}getpp
+┊✨${prefix}getbio
+┊✨${prefix}tagall
+┊✨${prefix}admin
+┊✨${prefix}fitnah
+┊✨${prefix}fitnahpc
+┊✨${prefix}kontak
+┊
+├───〔 ᴏᴡɴᴇʀ ᴍᴇɴᴜ 〕
+┊💎${prefix}off
+┊💎${prefix}on
+┊💎${prefix}buggc
+┊💎${prefix}bc
+┊💎${prefix}bcimage
+┊💎${prefix}bcgif
+┊💎${prefix}bcvideo
+┊💎${prefix}bcaudio
+┊💎${prefix}bcsticker
+┊💎${prefix}status
+┊💎${prefix}clearall
+┊💎${prefix}self
+┊💎${prefix}public
+┊💎${prefix}leave
+┊💎${prefix}clearall
+┊💎${prefix}setthumb
+┊💎${prefix}settarget
+┊💎${prefix}setfakeimg
+┊💎${prefix}setreply
+┊💎${prefix}term
+┊💎${prefix}setcmdmenu
+┊💎${prefix}setcmdping
+┊💎${prefix}setcmdplay
+┊💎${prefix}setcmdclose
+┊💎${prefix}setcmdopen
+┊💎${prefix}setcmditos
+┊💎${prefix}setcmdtoimg
+┊💎${prefix}setcmdself
+┊💎${prefix}setcmdpublic
+┊💎${prefix}upswteks
+┊💎${prefix}upswimage
+┊💎${prefix}upswvideo
+┊💎${prefix}addstik 
+┊💎${prefix}addimg 
+┊💎${prefix}addvid 
+┊💎${prefix}addvn   
+┊💎x or >
+┊
+├───〔 sᴛᴏʀᴀɢᴇ  ᴍᴇɴᴜ 〕
+┊📁${prefix}liststick
+┊📁${prefix}listimg
+┊📁${prefix}listvid
+┊📁${prefix}listvn
+┊
+├───〔 ᴛᴀɢ  ᴍᴇɴᴜ 〕
+┊🔖${prefix}hidetag
+┊🔖${prefix}kontag
+┊🔖${prefix}sticktag
+┊🔖${prefix}totag
+┊
+├───〔 ᴛʜᴀɴᴋs ᴛᴏ 〕
+┊
+├〔 sᴘʏʀᴏ sᴇʀ 〕
+├〔 ᴀᴅɪᴛʜ sᴇʀ 〕
+├〔 ᴛᴏᴍ sᴇʀ 〕
+├〔 ᴊɪɴɴ sᴇʀ 〕
+┊
+└───〔 sᴘʏʀᴏ 〕
+`
+}
+
+const maker = (prefix) => {
+	return`
+┌───〔 *_sᴘʏʀᴏ_* 〕
+┊
+└───────────𔒝
+┌───〔 ᴍᴀᴋᴇʀ ᴍᴇɴᴜ 〕
+┊🎐${prefix}sticker
+┊🎐${prefix}swm
+┊🎐${prefix}take
+┊🎐${prefix}fdeface
+┊🎐${prefix}emoji
+└───────────𔒝
+`
+}
+
+const other = (prefix) => {
+	return`
+┌───〔 *_sᴘʏʀᴏ_* 〕
+┊
+└───────────𔒝
+┌───〔 ᴏᴛʜᴇʀ ᴍᴇɴᴜ 〕
+┊✨${prefix}add
+┊✨${prefix}ping
+┊✨${prefix}get
+┊✨${prefix}getpp
+┊✨${prefix}getbio
+┊✨${prefix}tagall
+┊✨${prefix}admin
+┊✨${prefix}fitnah
+┊✨${prefix}fitnahpc
+┊✨${prefix}kontak
+└───────────𔒝
+`
+}
+
+const ownermenu = (prefix) => {
+	return`
+┌───〔 *_sᴘʏʀᴏ_* 〕
+┊
+└───────────𔒝
+┌───〔 ᴏᴡɴᴇʀ ᴍᴇɴᴜ 〕
+┊💎${prefix}off
+┊💎${prefix}on
+┊💎${prefix}buggc
+┊💎${prefix}bc
+┊💎${prefix}bcimage
+┊💎${prefix}bcgif
+┊💎${prefix}bcvideo
+┊💎${prefix}bcaudio
+┊💎${prefix}bcsticker
+┊💎${prefix}status
+┊💎${prefix}clearall
+┊💎${prefix}self
+┊💎${prefix}public
+┊💎${prefix}leave
+┊💎${prefix}clearall
+┊💎${prefix}setthumb
+┊💎${prefix}settarget
+┊💎${prefix}setfakeimg
+┊💎${prefix}setreply
+┊💎${prefix}term
+┊💎${prefix}setcmdmenu
+┊💎${prefix}setcmdping
+┊💎${prefix}setcmdplay
+┊💎${prefix}setcmdclose
+┊💎${prefix}setcmdopen
+┊💎${prefix}setcmditos
+┊💎${prefix}setcmdtoimg
+┊💎${prefix}setcmdself
+┊💎${prefix}setcmdpublic
+┊💎${prefix}upswteks
+┊💎${prefix}upswimage
+┊💎${prefix}upswvideo
+┊💎${prefix}addstik 
+┊💎${prefix}addimg 
+┊💎${prefix}addvid 
+┊💎${prefix}addvn   
+┊💎x or >
+└───────────𔒝
+`
+}
+
+
+const storage = (prefix) => {
+	return`
+┌───〔 *_sᴘʏʀᴏ_* 〕
+┊
+└───────────𔒝
+┌───〔 sᴛᴏʀᴀɢᴇ  ᴍᴇɴᴜ 〕
+┊📁${prefix}liststick
+┊📁${prefix}listimg
+┊📁${prefix}listvid
+┊📁${prefix}listvn
+└───────────𔒝
+`
+}
+
+const tag = (prefix) => {
+	return`
+┌───〔 *_sᴘʏʀᴏ_* 〕
+┊
+└───────────𔒝
+┌───〔 ᴛᴀɢ  ᴍᴇɴᴜ 〕
+┊🔖${prefix}hidetag
+┊🔖${prefix}kontag
+┊🔖${prefix}sticktag
+┊🔖${prefix}totag
+└───────────𔒝
+`
+}
+
+
+
+exports.help = help
+exports.convert = convert
+exports.download = download
+exports.maker = maker
+exports.other = other
+exports.ownermenu = ownermenu
+exports.storage = storage
+exports.tag = tag
+exports.allmenu = allmenu
